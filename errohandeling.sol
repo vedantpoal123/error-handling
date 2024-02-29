083a83b9bb68fd7cf136a394dd1702574c22e8fe
vedantpoal123 <161232675+vedantpoal123@users.noreply.github.com>
2024-02-29 23:47:00 +0530

    Update errohandeling.sol

--- errohandeling.sol
+++ errohandeling.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract ErrorHandling {
+
+    address public owner;
+    uint public balance;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // require
+    function setOwner(address _newOwner) public {
+        require(msg.sender == owner, "Only the current owner can change the owner");
+        owner = _newOwner;
+    }
+
+    // revert
+    error InsuffBal(uint balance, uint withdrawAmount);
+
+    function withdraw(uint _withdrawAmount) public view {
+        uint bal = address(this).balance;
+        if (bal < _withdrawAmount) {
+            revert InsuffBal({balance: bal, withdrawAmount: _withdrawAmount});
+        }
+    }
+    // assert
+    uint256 public total;
+
+    function addValue(uint256 _value) public {
+        total += _value;
+        assert(total > 0 && total <= 100);
+    }
+}
